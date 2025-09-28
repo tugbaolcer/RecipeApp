0901e879b73f34f20c63271b6f477a04dc2cd7fc
tugbaolcer <tugbaolcer52@gmail.com>
2025-09-28 17:40:04 +0300

    fixed: api adress

--- app/src/main/java/com/tugbaolcer/recipeapp/data/api/AppApi.kt
+++ app/src/main/java/com/tugbaolcer/recipeapp/data/api/AppApi.kt
@@ -5,6 +5,6 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface AppApi {
-    @GET("categories.ph")
+    @GET("categories.php")
     suspend fun getCategories(): Response<GetCategoriesResponse>
 }
